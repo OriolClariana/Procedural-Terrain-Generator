e43b3344ed722c10933e52d13d8d1a4aadd03b40
Oriol <oriolcj@gmail.com>
2019-02-09 12:05:11 +0000

    Project Blank

---
+++ TerrainGenerator/Source/TerrainGenerator/TerrainGeneratorGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TerrainGeneratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRAINGENERATOR_API ATerrainGeneratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
